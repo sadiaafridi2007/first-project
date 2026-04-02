e8c57af29669b9d0fc1abec47b0421ffe47fd10c
sadiaafridi2007 <sadiaafridi2007@gmail.com>
2026-04-02 18:39:56 +0500

    added power in calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 int main() {
 char op;
 double num1, num2;
 // Prompt user to enter operator and numbers
-cout<<"Enter operator: +, -, *, /: ";
+cout<<"Enter operator: +, -, *, / ,^: ";
 cin>>op;
 cout<<"Enter two operands:";
 cin>>num1>>num2;
@@ -14,7 +15,7 @@ case '+':
 cout<<num1<<" + "<< num2<<" = "<< num1 + num2;
 break;
 case '-':
-cout<<num1<<" - "<<" = "<< num1 - num2;
+cout<<num1<<" - "<<num2<<" = "<< num1 - num2;
 break;
 case '*':
 cout<< num1<<" * "<<num2<<" = "<<num1 * num2;
@@ -27,6 +28,8 @@ cout<<"Error! Division by zero is not allowed";
 cout<<num1<<" / "<< num2<<" = "<<num1 / num2;
 }
 break;
+  case'^':
+  cout<<num1<<" ^ "<<num2<<" = "<<pow(num1,num2);
 default:
 // Error message for invalid operator
 cout<<"Error! operator is not correct";
